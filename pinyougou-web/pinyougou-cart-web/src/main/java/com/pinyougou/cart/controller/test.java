bd5f7f6448a139272a740ca835f3e531856d15d1
18002 <475033985@qq.com>
2019-08-21 15:29:36 +0800

    添加了一个test

--- pinyougou-web/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/test.java
+++ pinyougou-web/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/test.java
@@ -3,5 +3,8 @@ package com.pinyougou.cart.controller;
 public class test {
     public static void main(String[] args) {
         System.out.println("bac");
+        System.out.println("bac");
+        System.out.println("bac");
+        System.out.println("bac");
     }
 }
